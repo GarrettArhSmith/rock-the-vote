46cab85ffa71f37a7f97491d648834cd31edb500
Garrett Smith <garrettarhsmith@gmail.com>
2021-06-14 20:48:06 -0400

    06/14/2021 - 1

--- client/src/components/Issue.js
+++ client/src/components/Issue.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Votes from './Votes'
+import styled from 'styled-components'
+
+const ReadMore = styled.span`
+    color: mediumblue;
+    &:hover {
+        text-decoration: underline;
+        cursor: pointer;
+    }
+`
 
 function Issue(props) {
     const { title, description, _id, user, upVoters, downVoters } = props
+    const [expanded, setExpanded] = useState(false)
 
-    function handleClick(e) {
+    // function handleClick(e) {
+    //     e.preventDefault()
+    //     e.stopPropagation()
+    // }
+
+    function handleExpand(e) {
         e.preventDefault()
-        e.stopPropagation()
+        setExpanded(prev => !prev)
     }
 
     return (
@@ -14,7 +29,19 @@ function Issue(props) {
             <div className="issueContent">
                 <h3>{title}</h3>
                 <p className="author">Posted by {user?.username}</p>
-                <p className="description">{description}</p>
+                <p className="description">
+                    {description.length > 450 && !expanded ? 
+                    <>
+                        {description.substring(0, 449) + "... "}
+                        <ReadMore onClick={handleExpand}>read more</ReadMore>
+                    </> : 
+                        description.length > 450 ? 
+                    <>
+                        {description} <ReadMore onClick={handleExpand}>read less</ReadMore>
+                    </> : 
+                    description
+                    }
+                </p>
             </div>
             <Votes _id={_id} votes={{upVotes: upVoters, downVotes: downVoters}} postType="issue" />
         </div>
